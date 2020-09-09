e8141b42d8de3dae2b7bfc3625862cbc23f5cadc
Nompilo Moyo <Nompilo.Moyo@Conlog.com>
2020-09-09 21:27:20 +0200

    Add project files.

---
+++ MyLinq.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Linq;
+
+namespace QueriesLinq
+{
+    public static class MyLinq
+    {
+        //quering infinity(create ienumerable method)
+        public static IEnumerable<double> Random()
+        {
+            var random = new Random();
+            while (true)
+            {
+                yield return random.NextDouble();
+            }
+        }
+
+
+        //creating a custom filter operator method as an extension
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source,
+            Func<T, bool> predicate)
+        {
+            foreach(var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace QueriesLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //querifying infinity
+            var numbers = MyLinq.Random().Where(n => n > 0.5).Take(10);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+            //create new movies list
+            var movies = new List<Movie>
+            {
+                new Movie {Title = "The Dark Knight", Rating = 8.0f, Year = 2020 },
+                new Movie {Title = "The King's Speech", Rating = 8.9f, Year = 2019 },
+                new Movie {Title = "Casablanka", Rating = 8.5f, Year = 1996 },
+                new Movie {Title = "Star Wars V", Rating = 8.7f, Year = 1999 }
+            };
+
+            //Query using an extensin method to filter movies(replace where with Filter to use filter method)
+            var query = movies.Filter(m => m.Year > 2000).ToList() 
+                .OrderByDescending(m=>m.Rating);
+            Console.WriteLine(query.Count());
+                //.Take(1);(when you want to look at only 1 year
+
+            /**foreach(var movie in query)
+            {
+                Console.WriteLine(movie.Title);
+            }**/
+
+            //Another way to write foreach statement
+            var enumerator = query.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Title);
+            }
+        }
+    }
+}
